1ad8e1ff7f265f8623207411bffe7bd445fc16a2
Elliot Winard <elliot.winard@frogdesign.com>
2014-07-02 10:30:58 -0400

    break up command-line args

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
-var argv = require('minimist')(process.argv.slice(2));
-console.dir(argv);
+var argv = require('minimist')(process.argv.slice(2)),
+     args = argv._,
+     action = args[0],
+     name = args[1],
+     value = args.slice(2).join(" "); // rest
+
+console.log(action, name, value);
+console.log(argv);
 
